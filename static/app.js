3fa076ef2d7f3b4b9221fb44b72c5129aa7ae704
L1L3V4 <eva.s284913@cesurformacion.com>
2022-12-19 11:48:17 +0100

    termiado

---
+++ static/app.js
@@ -0,0 +1,8 @@
+// Eva Téllez Bernal
+//
+// Completar JS aqui
+const form = document.getElementById('form-id');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  alert('Datos enviados con exito!');
+});
